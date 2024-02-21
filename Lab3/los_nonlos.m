ff913c67c1ee4e404806f0c4ef8be0fefcc00712
Arjun Ram <arjunram0369@gmail.com>
2024-02-22 00:03:45 +0530

    Los and Non Los

--- Lab3/los_nonlos.m
+++ Lab3/los_nonlos.m
@@ -16,6 +16,7 @@ field_size = [100 100];
 % Define the base station location
 base_station = [50 50];
 
+%Define all Parameter
 f = 900*(10^6);
 fraunhoferDistance = 10;
 c = 3*(10^8);
@@ -24,6 +25,8 @@ transmitt_power=10;
 gt = 1;
 gr = 1;
 
+
+%Take inut from user
 n = input("Enter no of nodes: ");
 
 
@@ -36,9 +39,10 @@ nodey = randi([1, field_size(2)], 1, n);
 node = [nodex; nodey];
 
 
+% Create a matrix to store the locations of the base station and nodes
  locations = [base_station; node.'];
 
-% Initialize a 6x6 matrix to store the distances
+% Initialize a matrix to store the distances
 distances = zeros(n+1,n+1);
 % 
 % Calculate the distances
@@ -64,6 +68,28 @@ disp(find(farFieldCondition));
 
 disp('Points at Fraunhofer Distance:');
 disp(find(fraunhoferFieldCondition));
+
+
+node_colors = cell(1, n+1);
+node_colors{1} = 'ro'; % Base station is always red
+for i = 2:n+1
+    if distances(1,i) <= fraunhoferDistance
+        % Fraunhofer Device
+        node_colors{i} = 'k*'; % Black color for Fraunhofer  field devices
+        received_powers(i) = 0; % Power is 0 for Fraunhofer  field devices
+    elseif distances(1,i) <= 40
+        % near field device (LOS)
+        node_colors{i} = 'g*'; % Green color for far field devices
+        % Calculate power using Friis equation considering reflections
+        received_powers(i) = (1000 * transmitt_power * gt * gr * (wavelength^2)) / ((4 * pi * distances(1,i))^2);
+    else
+        % Far field device (Non LOS)
+        node_colors{i} = 'b*'; % blue color for far field devices
+        % Calculate power using Friis equation considering reflections
+        received_powers(i) = (1000 * transmitt_power * gt * gr * (wavelength^2)) / ((4 * pi)^2 * distances(1,i))^4;
+    end
+end
+
 % Create a figure
 figure;
 
@@ -71,12 +97,16 @@ figure;
 plot([0, field_size(1), field_size(1), 0, 0], [0, 0, field_size(2), field_size(2), 0], 'k-');
 hold on;
 
+% Plot the base station
 
-scatter(node(1,:), node(2,:), 'b*');
-title('Random Points');
-xlabel('Index');
-ylabel('Value');
+scatter(base_station(1), base_station(2), 'ro', 'filled');
 
+% Plot the nodes
+for i = 2:n+1
+    scatter(node(1,i-1), node(2,i-1), node_colors{i});
+end
+
+%plot circle for different region
 viscircles([base_station(1), base_station(2)],10, 'Color','r', 'Linewidth', 0.5)
 viscircles([base_station(1), base_station(2)],40,'Color','b', 'Linewidth', 0.5)
 
@@ -93,11 +123,12 @@ title('Square Field with Base Station and Nodes');
 h(1) = plot(NaN,NaN,'-r', 'MarkerFaceColor', 'r');
 hold on;
 h(2) = plot(NaN,NaN,'-g', 'MarkerFaceColor', 'b');
-% h(3) = plot(NaN,NaN,'-b', 'MarkerFaceColor', 'b');
 
 % Create legend
 legend(h, 'Fraunhofer Area', 'Near Field or Los');
 
 % Show the grid
 grid on;
+
+
 
